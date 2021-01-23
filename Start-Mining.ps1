581b7382fff49a1b7173e8f4cf422b956dcbd928
BenHaslett <BenHaslett@gmail.com>
2021-01-23 12:09:42 +1100

    added phillpsHue temperature detection to stop mining when ambient heat get's to high

--- Start-Mining.ps1
+++ Start-Mining.ps1
@@ -5,6 +5,12 @@ $RunningProcs = Get-Process
 $busytest = @()
 $busy = $false
 
+$config = Import-LocalizedData -filename config.psd1
+$bridge = ("http://" + $config.ip)
+$user = $config.user
+
+$officetemp = ((Invoke-WebRequest "$bridge/api/$user/sensors/34").content | convertfrom-json).state.temperature
+
 function Write-Log{
     [CmdletBinding()]
     param (
@@ -30,8 +36,8 @@ foreach ($exe in $MiningProofApps.exe){
 
 $busy = $busytest.Running | Where-Object {$_-eq $true} | Select-Object -First 1
 
-if(!$busy){
-    Write-Log -LogEventText "Found no MiningProofApps Running so resuming mining"
+if(!$busy -and $officetemp -le "2900"){
+    Write-Log -LogEventText "Found no MiningProofApps and temp is $officetemp Running so resuming mining"
 	$MinerPaths | Select-Object -Property path, startcmd | ForEach-Object {
         $temp = $_
         $MiningProcessName = ((Split-Path -Leaf $_.path) -split ".exe")[0]
@@ -44,13 +50,13 @@ if(!$busy){
 				} else {
 				start-process -filepath $temp.path -WorkingDirectory (Split-Path -Parent $temp.path) -Verb runAs
 				}
-            Write-Log -LogEventText "Started $MiningProcessName"
+            Write-Log -LogEventText "Started $MiningProcessName temp is $officetemp"
         }
     }
 }
 else{
     $GameName = ($busytest | Where-Object {$_.Running -eq $true}).ProcessName
-    Write-Log -LogEventText "MiningProofApp $GameName running so halting Miners"
+    Write-Log -LogEventText "MiningProofApp $GameName running and temp is $officetemp so halting Miners "
     foreach ($path in $MinerPaths.Path){
         $MiningProcessName = ((Split-Path -Leaf $path) -split ".exe")[0]
         if($RunningProcs.ProcessName -contains $MiningProcessName ){
@@ -60,7 +66,7 @@ else{
             Write-Log -LogEventText $LogEventText
         }
         else{
-            Write-Log -LogEventText "$MiningProcessName not running"
+            Write-Log -LogEventText "$MiningProcessName not running temp is $officetemp"
         }
     }
 }
